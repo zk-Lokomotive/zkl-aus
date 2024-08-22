2f6862aa1d66d0ee2687da4cb2ae927da22a1248
Virjilakrum <asesenep15@gmail.com>
2024-08-22 15:50:47 +0300

    nodejs bug

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ArweaveUploaderTest from './ArweaveUploaderTest';
+
+function App() {
+  return (
+    <div className="App">
+      <ArweaveUploaderTest />
+    </div>
+  );
+}
+
+export default App;
