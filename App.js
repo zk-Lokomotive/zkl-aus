b8f0b993b9255727f1c5528277d5e9025cd207c6
Virjilakrum <asesenep15@gmail.com>
2024-08-22 16:00:57 +0300

    update

--- App.js
+++ App.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import ArweaveUploaderTest from './ArweaveUploaderTest';
+import ArweaveUploader from './ArweaveUploader';
 
 function App() {
   return (
     <div className="App">
+      <h1>Arweave Dosya Yükleme Uygulaması</h1>
+      <ArweaveUploader />
+      <hr />
+      <h2>Test Bileşeni</h2>
       <ArweaveUploaderTest />
     </div>
   );
